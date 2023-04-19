62780b1e68f23e8ffd3230a0d3e34653ad1571ee
Artur Gorevyi <108293399+Artur-Gorevyi@users.noreply.github.com>
2023-04-19 20:25:44 +0300

    Add files via upload

---
+++ Lab 8/simple-react-app/src/components/Expenses.js
@@ -0,0 +1,18 @@
+import ExpenseItem from './ExpenseItem';
+
+function Expenses(props) {
+  return (
+    <div className='card expenses'>
+      {props.expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Expenses;
